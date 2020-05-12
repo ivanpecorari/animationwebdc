b09974dfb9f65d80f4c825a4c5a1261ea8767b4c
ivanpecorari <37478457+ivanpecorari@users.noreply.github.com>
2020-05-12 11:10:22 +0100

    update business

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animations = ['startups','business','investors','public','research',];
+var animations = ['startups','business3','investors','public','research',];
     var i, len = animations.length;
     for(i = 0; i < len; i += 1) {
         var anim;
