141e82ea159fda78ac7be84e73b0efbe34413d2b
ivanpecorari <37478457+ivanpecorari@users.noreply.github.com>
2020-05-12 09:23:39 +0100

    latest version

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'research.json',
-    
-})
+var animations = ['startups','business','investors','public','research',];
+    var i, len = animations.length;
+    for(i = 0; i < len; i += 1) {
+        var anim;
+        var elem = document.getElementById(animations[i])
+        var animData = {
+            container: elem,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            rendererSettings: {
+                progressiveLoad:true,
+                preserveAspectRatio:'xMidYMid meet'
+            },
+            path: animations[i]+'.json'
+        };
+        anim = lottie.loadAnimation(animData);
+        anim.setSubframe(false);
+    }
